c93f66fafbef9a6c33a2544f1f8d7cda859e14d0
James Grieve <jamesonrgrieve@gmail.com>
2024-01-23 10:33:48 -0700

    Fixed suspense boundary;

--- next.config.js
+++ next.config.js
@@ -2,23 +2,21 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
-    //experimental line to remove Suspense with CSR bailout warning when deploying or trying to build. Documentation found here. (https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout)
-    missingSuspenseWithCSRBailout: false,
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ["lh3.googleusercontent.com"],
   },
   webpack(config) {
-    config.experiments = {
+    (config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    },
-      config.resolve.fallback = {
+    }),
+      (config.resolve.fallback = {
         "mongodb-client-encryption": false,
-        "aws4": false
-      };
-    return config
-  }
-}
+        aws4: false,
+      });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
