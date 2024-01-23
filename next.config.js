2fe0cb4df2358f70acb81b5afe80188b7ed2ecd8
James Grieve <jamesonrgrieve@gmail.com>
2024-01-23 11:25:36 -0700

    Add linting configuration; Add Next bundle analyzer;

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || "NextJS Application",
+    NEXT_PUBLIC_APP_NAME: process.env.APP_NAME || 'NextJS Application',
   },
   experimental: {
-    serverComponentsExternalPackages: ["mongoose"],
+    serverComponentsExternalPackages: ['mongoose'],
   },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ['lh3.googleusercontent.com'],
   },
   webpack(config) {
     (config.experiments = {
@@ -15,11 +15,14 @@ const nextConfig = {
       topLevelAwait: true,
     }),
       (config.resolve.fallback = {
-        "mongodb-client-encryption": false,
+        'mongodb-client-encryption': false,
         aws4: false,
       });
     return config;
   },
 };
-
-module.exports = nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: true,
+});
+module.exports = process.env.NEXT_ANALYZE === 'true' ? withBundleAnalyzer(nextConfig) : nextConfig;
