330b922ef8d91f3dbe438431fa4ac491a02090a7
Larnerk <97647756+Larnerk@users.noreply.github.com>
2024-01-22 21:23:54 -0700

    Changes to next.config.js, add comment for documentation.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["mongoose"],
+    //experimental line to remove Suspense with CSR bailout warning when deploying or trying to build. Documentation found here. (https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout)
     missingSuspenseWithCSRBailout: false,
   },
   images: {
