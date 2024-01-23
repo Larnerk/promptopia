8788c4a2abb489d0b487519630b2be9fe944b6d8
James Grieve <jamesonrgrieve@gmail.com>
2024-01-23 11:27:37 -0700

    Removed errant TS comment; Removed React must be in scope;

--- next.config.js
+++ next.config.js
@@ -21,7 +21,6 @@ const nextConfig = {
     return config;
   },
 };
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: true,
 });
